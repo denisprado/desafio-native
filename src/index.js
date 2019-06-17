a14f4473fd28a813351cc4c554a9d72ee3d37a67
Denis Forigo <denisforigo@gmail.com>
2019-06-16 23:14:13 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
